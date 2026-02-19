fc7667e08663f7e3064732745fda8620b42709a7
WolfCipher <rhowardsmith@wisc.edu>
2026-02-19 09:02:06 -0600

    inputs and outpus

--- project3/rtl/hart.v
+++ project3/rtl/hart.v
@@ -134,32 +134,34 @@ module hart #(
 endmodule
 
 // PC, instruction memory
-module fetch();
+module fetch(
+);
 
 endmodule
 
 // control unit, register file, immediate decoder
 // does NOT choose between immediate and register 2
-module decode();
+module decode(
+);
 
 endmodule
 
 // alu, branch/jump update
 // DOES choose between immediate and register 2
 module execute(
-    input wire [31:0] reg1
-    input wire [31:0] reg2
-    input wire [31:0] imm
-    input wire ALUSrc
-    input wire [2:0] i_opsel
-    input wire i_sub
-    input wire i_unsigned
-    input wire i_arith
-    input wire MemRead1
-    output wire [31:0] o_result
-    output wire o_eq
-    output wire o_slt
-    output wire target_addr
+    input wire [31:0] reg1,
+    input wire [31:0] reg2,
+    input wire [31:0] imm,
+    input wire ALUSrc,
+    input wire [2:0] i_opsel,
+    input wire i_sub,
+    input wire i_unsigned,
+    input wire i_arith,
+    input wire MemRead1,
+    output wire [31:0] o_result,
+    output wire o_eq,
+    output wire o_slt,
+    output wire target_addr,
     output wire MemRead2
 );
 
@@ -167,14 +169,14 @@ endmodule
 
 // data memory
 module memory(
-    input wire [31:0] o_result
-    input wire o_eq
-    input wire o_slt
-    input wire target_addr
-    input wire MemRead1
-    output wire [31:0] PC
-    output wire [31:0] read_data
-    output wire [31:0] read_alu
+    input wire [31:0] o_result,
+    input wire o_eq,
+    input wire o_slt,
+    input wire target_addr,
+    input wire MemRead1,
+    output wire [31:0] PC,
+    output wire [31:0] read_data,
+    output wire [31:0] read_alu,
     output wire MemRead2
 );
 
@@ -182,10 +184,10 @@ endmodule
 
 
 module writeback(
-    input wire [31:0] PC
-    input wire [31:0] read_data
-    input wire [31:0] read_alu
-    input wire MemRead
+    input wire [31:0] i_PC,
+    input wire [31:0] read_data,
+    input wire [31:0] read_alu,
+    input wire MemRead,
     output wire [31:0] dest_result
 );
 
