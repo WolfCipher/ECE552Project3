146cab188c273d16b67b463926ff1bd8c6932cf1
zahabiyah-1 <zahabiyahboty3@gmail.com>
2026-02-25 10:55:31 -0600

    fetch

--- project3/rtl/fetch.v
+++ project3/rtl/fetch.v
@@ -1,23 +1,18 @@
-
+  
 module fetch #(
     parameter RESET_ADDR = 32'h00000000
 ) (
-    input i_rst, i_clk,
-	input [31:0] branch_target,
-	input branch_taken,
+    input i_rst, 
+	input i_clk,
 	input wire [31:0] i_imem_rdata, // data from mem
+	input wire [31:0] next_pc,
 	output wire [31:0] o_imem_raddr,
 	output reg [31:0] pc,
 	output wire [31:0] instruction
 );
-	wire[31:0] next_pc;
-	wire[31:0] pc_incr;
 
-	assign pc_incr = pc + 4;
-	assign next_pc = branch_taken ? branch_target : pc_incr;
-	assign o_imem_raddr = pc; // read at the addr not the next one
 	assign instruction = i_imem_rdata; // what the instruction says
-
+	assign o_imem_raddr = pc;
 
     always @(posedge i_clk) begin
         if (i_rst) pc <= RESET_ADDR; // if we need to rset set to base addr
